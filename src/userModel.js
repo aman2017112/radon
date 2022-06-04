e0b927692df12296ad16c399874e1d5e78fabf9a
aman2017112 <agrawalaman971@gmail.com>
2022-06-04 11:03:09 +0530

    demo1

---
+++ src/userModel.js
@@ -0,0 +1,21 @@
+// const mongoose = require('mongoose');
+// const userScheme = new mongoose.Schema({
+//     firstName : String,
+//     last name : String,
+//     mobile : {
+//             type : String,
+//             unique : true,
+//             required : true
+
+
+//     },
+//     emailid : String,
+//     gender : {
+//         type : String,
+//          enum : ["male","female","LGBTQ"]
+
+//     },
+//     age : Number
+// }, { timestamps: true});
+
+// module.exports = mongoose.model('User', userScheme)   //users
