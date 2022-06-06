16a1f19b0ab59613f27173a77bfd22fae52a9ad6
aman2017112 <agrawalaman971@gmail.com>
2022-06-06 12:51:04 +0530

    model1

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+
+mongoose.connect("mongodb+srv://aman19992017:aman2017112@cluster0.gwibvox.mongodb.net/amanagrawal-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
