c132302915d65ec747faa3d0a6f08e9ed60d9da1
aman2017112 <agrawalaman971@gmail.com>
2022-06-07 22:34:57 +0530

    assignment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://aman19992017:aman2017112@cluster0.gwibvox.mongodb.net/author-book-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
